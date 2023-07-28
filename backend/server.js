7caf8757cd5cf5fe18d51fb626e3ccb51fca8d21
chandramouli <chandramoulichandru9741@gmail.com>
2023-07-28 07:39:36 +0530

    Created api endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
 require('dotenv').config();
 const express = require('express');
+const workoutRouter = require('./routes/workouts')
 
 //express app
 const app = express();
 
-app.get("/", (req, res) => {
-    res.json({mssg: "welcome to the app"});
+//middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
 });
+
+//routes
+app.use("/api/workouts", workoutRouter);
+
 
 // Listen for requests
 app.listen(process.env.PORT, () => {

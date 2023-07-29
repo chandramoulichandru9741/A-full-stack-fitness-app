0dc0977bf5805e03fd8f4d807a3cd23510a0d043
chandramouli <chandramoulichandru9741@gmail.com>
2023-07-29 13:33:54 +0530

    Created database model and controllers

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRouter = require('./routes/workouts')
 
 //express app
@@ -16,6 +17,12 @@ app.use((req, res, next) => {
 //routes
 app.use("/api/workouts", workoutRouter);
 
+//connect to db
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {})
+.catch((error) => {
+    console.log(error);
+})
 
 // Listen for requests
 app.listen(process.env.PORT, () => {

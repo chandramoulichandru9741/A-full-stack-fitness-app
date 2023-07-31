6a50b756a3cec8f24e2f41ada6c3725beeaf6d98
chandramouli <chandramoulichandru9741@gmail.com>
2023-07-31 21:21:02 +0530

    added useState and useEffect in home.js file

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,4 +1,17 @@
+import { useEffect, useState } from 'react';
+
 const Home = () => {
+    const [workouts, setWorkouts] = useState(null);
+    useEffect(() => {
+        const fetchWorkout = async () => {
+            const response = await fetch('http://localhose:4000/api/workouts');
+            const json = await response.json()
+
+            if (response.ok){
+                setWorkouts(json)
+            }
+        }
+    }, []);
     return (
         <div className="home">
             <h2>Home</h2>

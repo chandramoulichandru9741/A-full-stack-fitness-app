c94a705a2dc90b8e90ff00c3d407dff6d65020a6
chandramouli <chandramoulichandru9741@gmail.com>
2023-08-01 21:42:07 +0530

    created workout details file

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
 
+//Components
+import WorkoutDetails from '../components/WorkoutDetails';
 const Home = () => {
     const [workouts, setWorkouts] = useState(null);
     useEffect(() => {
         const fetchWorkout = async () => {
-            const response = await fetch('http://localhose:4000/api/workouts');
+            const response = await fetch('/api/workouts');
             const json = await response.json()
 
             if (response.ok){
@@ -14,7 +16,11 @@ const Home = () => {
     }, []);
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout) => {
+                    <WorkoutDetails key={workout._id} workout={workout}/>
+                })}
+            </div>
         </div>
     )
 }

c43e12f819ab030d6c9096ecd8fa4cf3deb04852
chandramouli <chandramoulichandru9741@gmail.com>
2023-08-09 23:05:11 +0530

    added hook and delete functionalies

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { WorkoutsContextPovider } from './context/workoutContext';
+import { WorkoutsContextPovider } from './context/WorkoutsContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
